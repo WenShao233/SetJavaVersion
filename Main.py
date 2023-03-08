7eb9d1040011389bb47d7d9acc409a0b50f0e497
WenShao233 <118185772+WenShao233@users.noreply.github.com>
2023-03-08 21:22:41 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,66 +1,61 @@
-class SetJavaVersion:
-    def __init__(self) -> None:
-        import os
-        import json
-        self.JavaList = {}
-        self.JavaVersionList = {}
-        self.JavaPath = {}
-        self.AppData = os.environ.get("AppData")
-        print("欢迎使用Java切换器! 建议以管理员身份运行, 在可输入的地方输入help查看帮助")
-        with open(f"{self.AppData}\\SetJavaVersion\\JavaPath.json", "r+") as f1:
-            self.JavaPath = json.loads(f1.read(), strict = False)
-        if not os.path.exists(f"{self.AppData}\\SetJavaVersion"):
-            os.mkdir(f"{self.AppData}\\SetJavaVersion")
-            self.FirstStart()
-        elif not os.path.exists(f"{self.AppData}\\SetJavaVersion\\JavaPath.json") or self.JavaPath == "":
-            print("配置文件已丢失, 请重新配置")
-            self.FirstStart()
+import os
+import time
+import json
+
+print("欢迎使用Java切换器! 若有疑问, 可在任意可输入的地方输入help获得帮助文档")
+
+t1 = 0 
+JavaVersionList = []
+JavaList = {}
+
+for i in range(1, 21):
+    if os.environ.get(f"JAVA{i}_HOME") != None:
+        JavaVersionList.append(i)
+    else:
+        t1 += 1
+if t1 == 20:
+    n1 = input("请问你要添加多少个Java: ")
+    for j in range(n1):
+        JavaVersion = input(f"请输入第{j + 1}个Java版本号(Java8就写8, Java17就写17): ")
+        VersionPath = input(f"请输入第{j + 1}个Java目录: ")
+        os.system(f"setx \"JAVA{JavaVersion}_HOME\" \"{VersionPath}\"")
+
+try:
+    if os.environ.get("JAVA_HOME") != None:
+        JAVA_HOME = os.environ.get("JAVA_HOME")
+except:
+    pass
+finally:
+    t1 = 0
+    for y in JavaVersionList:
+        if os.environ.get(f"JAVA{y}_HOME") == JAVA_HOME:
+            print(f"当前Java为{y}, 位于{JAVA_HOME}")
+            continue
         else:
-            self.SettingJavaVersion()
+            t1 += 1
+    if t1 == len(JavaVersionList):
+        print("当前JAVA变量不正常, 请立即更换!")
 
-
-    def help():
-        print("""
+version = input("请输入要切换的Java版本: ")
+if version == "help":
+    print(
+        """
 帮助文档
 常用指令:
-    1. help
-    打开帮助文档
-    2. add
-    打开 [添加新的Java] 面板
-    3. remove
-    打开 [移除一个Java] 面板
-    4. reset
-    移除所有该程序修改过的系统变量(不包括JAVA_HOME)
+1. help
+打开帮助文档
+2. add
+打开 [添加新的Java] 面板
+3. remove
+打开 [移除一个Java] 面板
+4. reset
+移除所有该程序修改过的系统变量(不包括JAVA_HOME)
+        """
+        )
 
-Java路径保存位置: 
-    %AppData%\\SetJavaVersion
+if os.environ.get(f"JAVA{version}_HOME") == None:
+    print("当前没有该Java, 若您已安装该Java, 请输入help查看帮助文档")
+else:
+    os.system(f"setx \"JAVA_HOME\" \"%JAVA{version}_HOME%\"")
 
-声明: 该程序由Wen_Shao制作, 使用程序或源码时请标明出处
-            """)
-
-    def FirstStart(self):
-        import json
-        n1 = input("请问你要添加多少个Java: ")
-        for j in range(int(n1)):
-            JavaVersion = input(f"请输入第{j + 1}个Java版本号(Java8就写8, Java17就写17): ")
-            VersionPath = input(f"请输入第{j + 1}个Java目录(例: C:\Program Files\Java\Jre_8u231): ")
-        self.JavaPath[f"{JavaVersion}"] = VersionPath
-        with open(f"{self.AppData}\\SetJavaVersion\\JavaPath.json", "w+") as f1:
-            json.dump(self.JavaPath, f1, indent = 4)
-        self.SettingJavaVersion()
-        
-
-    def SettingJavaVersion(self):
-        import os
-        import time
-        version = input("请输入要切换的Java版本: ")
-        print("正在更改用户变量...")
-        os.system(f"setx \"JAVA_HOME\" \"{self.JavaPath[version]}")
-        print("\n")
-        print("正在更改系统变量...")
-        os.system(f"setx -m \"JAVA_HOME\" \"{self.JavaPath[version]}")
-        time.sleep(2)
-        exit()
-
-if __name__ == "__main__":
-    SetJavaVersion()
+time.sleep(2)
